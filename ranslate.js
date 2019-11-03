25c6aa53a4475a25fa6610822ffc38fe3c11ae00
tianrenwu <1051626281@qq.com>
2019-11-03 12:24:14 +0800

    first

---
+++ ranslate.js
@@ -0,0 +1,12 @@
+var timer = setTimeout(function(){
+    $('.wrapper').removeClass('init');
+},200)
+$('.item1').click(function(){
+    $(this).addClass('active');
+    $('.wrapper').addClass('wrapper-active');
+})
+$('.close').click(function(e){
+    e.stopPropagation();
+    $('.wrapper-active').removeClass('wrapper-active');
+    $('.active').removeClass('active');
+})
